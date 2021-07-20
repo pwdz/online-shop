8c9e791e4cfaec384c8a8adf9d8d918487132c6f
Mohammad ebrahim Adibzadeh <me.adibzadeh@gmail.com>
2021-07-21 01:17:52 +0430

    Fix slider

--- frontend/assets/script/slider.js
+++ frontend/assets/script/slider.js
@@ -1,28 +1,27 @@
-const base_path = "../images/slideshow/"
+const basePath = "assets/images/slideshow/"
 const images = ["1.jpg", "2.jpg", "3.jpg"];
 
 var currIndex = 0
-// showSlides(currIndex);
+showSlides(currIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(currIndex + n);
+  currIndex += n 
+  currIndex = currIndex % images.length
+  if(currIndex<0)
+    currIndex = images.length - 1
+
+  document.getElementById("imageslider").style.backgroundImage =  "url("+basePath + images[currIndex] + ")";
 }
 
-function showSlides(n) {
-
-  document.getElementById('imageslider').style.backgroundImage="url(../images/slideshow/3.jpg)"; // specify the image path here
+async function showSlides(n) {
   n = n % images.length;
   currIndex = n
 
-  var sliderComponent = document.getElementById("imageslider");
+  document.getElementById("imageslider").style.backgroundImage =  "url("+basePath + images[currIndex] + ")";
 
-  console.log(base_path + images[n]);
-  // sliderComponent.style.backgroundImage = "url("+base_path + images[n] + ")";
-  sliderComponent.style.backgroundImage = "url(../images/slideshow/1.jpg)";
-  console.log(sliderComponent)
-
-  // await sleep(10000);
+  await sleep(10000);
+  showSlides(n+1)
 } 
 
 function sleep(ms) {
